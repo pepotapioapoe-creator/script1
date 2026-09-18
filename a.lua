d5c59979fd48ec54bc91f6aec081aca822f63e38
pepotapioapoe-creator <pepotapioapoe@gmail.com>
2026-09-18 14:22:35 -0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,3 +1,12 @@
 local g = Instance.new("ScreenGui")
-g.Name = "TestGui123"
+g.Name = "TestGui456"
 g.Parent = game:GetService("Players").LocalPlayer.PlayerGui
+for i = 1, 500 do
+    local f = Instance.new("Frame")
+    f.Size = UDim2.new(0, 100, 0, 30)
+    f.Parent = g
+    local t = Instance.new("TextLabel")
+    t.Text = "Option " .. i
+    t.Parent = f
+end
+print("done")
